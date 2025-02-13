fd36999f83b8596f8bd6905a9f27d1305702dd36
vinaykumar <66993050+vinaykumar0339@users.noreply.github.com>
2025-02-13 11:59:00 +0530

    Initial Commit

---
+++ IntuneRemediationFlow/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IntuneRemediationFlow
+//
+//  Created by vinay kumar on 13/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

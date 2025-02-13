e8347722a37f12078672dc7d5bc3dee899e26252
vinaykumar <66993050+vinaykumar0339@users.noreply.github.com>
2025-02-13 15:38:24 +0530

    Add demo app for to test intuneremediation flow

--- IntuneRemediationFlow/ViewController.swift
+++ IntuneRemediationFlow/ViewController.swift
@@ -6,14 +6,177 @@
 //
 
 import UIKit
+import MSAL
+import IntuneMAMSwift
+
+
 
 class ViewController: UIViewController {
-
+    
+    private var msalApplication: MSALPublicClientApplication?
+    
+    let kClientID = "9794dec7-21df-48e9-8504-e4a01adf63f3"
+    let kRedirectUri = "msauth.com.test.IntuneRemediationFlow://auth"
+    let kAuthority = "https://login.microsoftonline.com/organizations"
+    let scopes = ["https://graph.microsoft.com/InformationProtectionPolicy.Read"]
+    private var accoundID: String? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        initializeMSAL()
+        signInButton()
+        signOutButton()
     }
+    
+    private func initializeMSAL() {
+        do {
+            let authority = try? MSALAuthority(url: URL(string: kAuthority)!)
+            let config = MSALPublicClientApplicationConfig(clientId: kClientID, redirectUri: kRedirectUri, authority: authority)
+            config.clientApplicationCapabilities = ["ProtApp"]
+            
+            self.msalApplication = try MSALPublicClientApplication(configuration: config)
+            
+            IntuneMAMSettings.aadClientIdOverride = kClientID
+            IntuneMAMSettings.aadAuthorityUriOverride = kAuthority
+            IntuneMAMSettings.aadRedirectUriOverride = kRedirectUri
+            
+            IntuneMAMEnrollmentManager.instance().delegate = self
+            IntuneMAMPolicyManager.instance().delegate = self
+            IntuneMAMComplianceManager.instance().delegate = self
+            
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    
+    private func signInButton() {
+        // Create a button
+        let button = UIButton(type: .system)
+        button.setTitle("Sign In", for: .normal)
+        button.backgroundColor = UIColor.systemBlue
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 10
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        // Add action to button
+        button.addTarget(self, action: #selector(signIn), for: .touchUpInside)
+        
+        // Add button to the view
+        view.addSubview(button)
+        
+        // Set constraints to center the button
+        NSLayoutConstraint.activate([
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            button.widthAnchor.constraint(equalToConstant: 150),
+            button.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    private func signOutButton() {
+        // Create a sign-out button
+        let button = UIButton(type: .system)
+        button.setTitle("Sign Out", for: .normal)
+        button.backgroundColor = UIColor.systemRed
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 10
+        button.translatesAutoresizingMaskIntoConstraints = false
 
+        // Add action to button
+        button.addTarget(self, action: #selector(signOut), for: .touchUpInside)
 
+        // Add button to the view
+        view.addSubview(button)
+
+        // Set constraints to place the button below the Sign In button
+        NSLayoutConstraint.activate([
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.topAnchor.constraint(equalTo: view.centerYAnchor, constant: 70), // Positioned below Sign In
+            button.widthAnchor.constraint(equalToConstant: 150),
+            button.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    @objc func signIn() {
+        guard let msalApplication = msalApplication else {
+            print("call initializeMSAL before using msalApplication")
+            return
+        }
+        
+        DispatchQueue.main.async { [weak self] in
+            
+            guard let strongSelf = self else {
+                print("self is nil")
+                return
+            }
+            
+            let webParameters = MSALWebviewParameters(authPresentationViewController: strongSelf)
+            
+            let interactiveParameters = MSALInteractiveTokenParameters(scopes: strongSelf.scopes, webviewParameters: webParameters)
+            interactiveParameters.promptType = .selectAccount
+            
+            msalApplication.acquireToken(with: interactiveParameters) { result, error in
+                guard let authResult = result, error == nil else {
+                    let nsError = error! as NSError
+                    if nsError.domain == MSALErrorDomain, let errorCode = MSALError(rawValue: nsError.code)
+                    {
+                        switch errorCode
+                        {
+                        case .serverProtectionPoliciesRequired:
+                            // Integrate the Intune SDK and call the
+                            // remediateComplianceForIdentity:silent: API.
+                            // Handle this error only if you integrated Intune SDK.
+                            // See more info here: https://aka.ms/intuneMAMSDK
+                            let homeAccoundId = nsError.userInfo[MSALHomeAccountIdKey] as! String
+                            let oid = homeAccoundId.split(separator: ".").first!
+                            self?.accoundID = String(oid)
+                            IntuneMAMComplianceManager.instance().remediateCompliance(forAccountId: String(oid), silent: true)
+                            break
+                        default:
+                            print("Failed with unknown MSAL error \(error?.localizedDescription ?? "No Error")")
+                        }
+                    }
+                    return
+                }
+                self?.accoundID = authResult.tenantProfile.identifier
+                print("success token: \(authResult.accessToken)")
+            }
+        }
+    }
+    
+    @objc func signOut() {
+        guard let accountId = accoundID else {
+            print("account id not found")
+            return
+        }
+        IntuneMAMEnrollmentManager.instance().deRegisterAndUnenrollAccountId(accountId, withWipe: true)
+    }
+    
+    
+}
+
+extension ViewController: IntuneMAMEnrollmentDelegate, IntuneMAMPolicyDelegate, IntuneMAMComplianceDelegate {
+    func restartApplication() -> Bool {
+        return true
+    }
+    
+    func wipeData(forAccountId accountId: String) -> Bool {
+        return true
+    }
+    
+    func enrollmentRequest(with status: IntuneMAMEnrollmentStatus) {
+        print(status.statusCode, status.errorString ?? "No error", "enrollmentRequest")
+    }
+    
+    func unenrollRequest(with status: IntuneMAMEnrollmentStatus) {
+        print(status.statusCode, status.errorString ?? "No error", "unenrollRequest")
+    }
+    
+    func accountId(_ accountId: String, hasComplianceStatus status: IntuneMAMComplianceStatus, withErrorMessage errMsg: String, andErrorTitle errTitle: String) {
+        print(accountId, status.rawValue, errMsg, errTitle, "IntuneMAMComplianceDelegate callback")
+    }
 }
 
